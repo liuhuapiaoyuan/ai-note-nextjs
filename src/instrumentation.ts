7a977a6a78df50c9d94236e973085765e7a5c2c3
liuhuapiaoyuan <278780765@qq.com>
2024-05-22 21:20:09 +0800

    fix: 没思路 下回再做

---
+++ src/instrumentation.ts
@@ -0,0 +1,4 @@
+export async function register() {
+
+  console.log("定时任务ss11启动了")
+}
--- src/lib/langchain/loaders/SimpleWebLoader.tsx
+++ src/lib/langchain/loaders/SimpleWebLoader.tsx
@@ -1,28 +1,36 @@
 
 
-
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 import { HtmlToTextTransformer } from "@langchain/community/document_transformers/html_to_text";
 
 
-export async function GET() {
+
+
+async function SimpleWebLoader(url: string, selector: string) {
   const loader = new CheerioWebBaseLoader(
-    "https://doc.fastadmin.net/developer/79.html",
+    url,
     {
       selector: ".content.with-sidebar"
     }
   );
   const docs = await loader.load();
+
   const splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
     chunkSize: 1000
   });
   const transformer = new HtmlToTextTransformer();
   const sequence = splitter.pipe(transformer);
-  const newDocuments = await sequence.invoke(docs);
-  return Response.json({
-    data: newDocuments.map((doc) => (doc.pageContent)),
-  });
-
+  return await sequence.invoke(docs);
+}
+/**
+ * 批量扫描
+ * @param urls 
+ * @param selector 
+ * @returns 
+ */
+export async function BatchSimpleWebLoader(urls: string[], selector: string) {
+  const docs = await Promise.all(urls.map(url => SimpleWebLoader(url, selector)));
+  return docs.flat();
 }
